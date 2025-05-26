cadb7fdac0b93c0f3621b5b1df2748f15977fe09
Natanael Arndt <arndtn@gmail.com>
2025-05-26 17:16:56 +0200

    Ad some more things to the test

--- tests/test_wapmh.py
+++ tests/test_wapmh.py
@@ -1,6 +1,6 @@
 from wapmh.repository import app
 from fastapi.testclient import TestClient
-
+from loguru import logger
 
 client = TestClient(app)
 
@@ -8,3 +8,6 @@ client = TestClient(app)
 def test_list_records():
     response = client.get("/", params={"verb": "ListRecords"})
     assert response.status_code == 200
+    if "<rdf:RDF" not in response.text:
+        logger.info(f"response:\n{response.text}")
+    assert "<rdf:RDF" in response.text
