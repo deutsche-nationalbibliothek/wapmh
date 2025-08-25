a916085c9761214c23ecc8ac0add074352873064
Natanael Arndt <arndtn@gmail.com>
2025-08-25 12:19:58 +0200

    Load config from default.env file

--- wapmh/config.py
+++ wapmh/config.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 class Settings(BaseSettings):
     repository_name: str = "Webarchive OAI-PMH Endpoint"
-    admin_emails: Optional[list[str]] = None
     description: str = "This is the OAI-PMH endpoint of the Webarchive."
+    admin_emails: Optional[list[str]] = None
 
     sparql_endpoint: str = "http://localhost:5000/query"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="default.env")
